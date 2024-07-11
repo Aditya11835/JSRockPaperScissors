d1b62f2f9a0b561bc2d57d510b0a4ce2cdee28f9
aditya11835 <143714442+Aditya11835@users.noreply.github.com>
2024-07-11 21:38:49 +0530

    Edit JS and HTML file to add UI

--- code.js
+++ code.js
@@ -9,94 +9,28 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let humanHand = prompt("Enter Rock, Papers, or Scissors", "Rock, Paper, or Scissors?");
-    humanHand = humanHand.toUpperCase();
-    switch(humanHand)
+function calculateRound(playerChoice, computerChoice)
+{
+    if(playerChoice === computerChoice)
     {
-        case "ROCK": return "Rock";
-        case "PAPER": return "Paper";
-        case "SCISSORS": return "Scissors";
-        default: return "INVALID";
+        return "Tie";
     }
-}
-
-function playSingleRound(){
-    alert("Enter your choice.")
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-
-    if(humanChoice === computerChoice)
-        {
-            console.log("TIE!");
-            console.log("No change in score.");
-        }
-    else if(humanChoice === "Rock")
-        {
-            if(computerChoice === "Paper")
-                {
-                    console.log("Paper beats Rock! Computer scores!");
-                    computerScore++;
-                }
-            else{
-                console.log("Rock beats Scissors! Player wins!");
-                humanScore++;
-            }
-        }
-    else if(humanChoice === "Paper")
-        {
-            if(computerChoice === "Rock")
-                {                     
-                    console.log("Paper beats Rock! Player scores!");
-                    humanScore++;
-                }
-            else{
-                    console.log("Scissors beat Rock! Computer wins!");
-                    computerScore++;    
-                }
-        }
-    else if(humanChoice === "Scissors")
-        {
-            if(computerChoice === "Paper")
-                {
-                    console.log("Scissors beat Paper! Player scores!");
-                    humanScore++;
-                }
-            else{
-                    console.log("Rock beats Scissors! Player wins!");
-                    computerScore++;
-                }
-        }
-
-    console.log("Player Score = " + humanScore);
-    console.log("Computer Score = " + computerScore);
-}
-
-function playGame(){
-    
-    for(i = 0; i<5; i++)
-        {
-            playSingleRound();
-        }
-    if(humanScore === computerScore)
-        {
-            console.log("Score Tied.");
-        }
-    else if(humanScore > computerScore)
-        {
-            console.log("Player wins!");
-        }
     else{
-            console.log("Computer wins!");
+        
     }
-
-    console.log("Player Score = " + humanScore);
-    console.log("Computer Score = " + computerScore);
-
 }
 
-let humanScore = 0;
-let computerScore = 0;
-playGame();
+function playRound(pressedButton){
+    let playerChoice = pressedButton;
+    let computerChoice = getComputerChoice();
+    let currentResult = calculateRound(playerChoice, computerChoice);
+}
 
+const inputButtonContainer = document.querySelector(".inputButtons");
+inputButtonContainer.addEventListener("click", (e)=>{
+    playRound(e.target.textContent);
+});
+
+let computerScore = 0;
+let humanScore = 0;
 
